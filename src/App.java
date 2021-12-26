bd1b71d1d1e3b086b2a2aa311110862464f1a099
imadsyed333 <64811136+imadsyed333@users.noreply.github.com>
2021-12-26 00:00:30 -0500

    Added App.java

---
+++ src/App.java
@@ -0,0 +1,11 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application{
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        //TODO
+    }
+    
+}
